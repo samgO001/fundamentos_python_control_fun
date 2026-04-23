fc5e1f9929ab0d2c46cda6ebffa4baba18125d8c
SamuelG <guarin090vvv@gmail.com>
2026-04-23 15:54:31 -0500

    feat: ejercicio 01 - declaración if básica

---
+++ src/condicionales/01_if_basico.py
@@ -0,0 +1,19 @@
+"""
+Ejercicio 1 – Declaración if básica
+Conceptos: if simple, operadores de comparación
+"""
+
+# Ejemplo 1: Mayor de edad
+edad = 20
+if edad >= 18:
+    print("Eres mayor de edad.")
+
+# Ejemplo 2: Temperatura
+temperatura = 30
+if temperatura > 25:
+    print("Hace calor hoy.")
+
+# Ejemplo 3: Operadores lógicos básicos con if
+hora = 10
+if hora >= 6 and hora < 12:
+    print("Buenos días.")
